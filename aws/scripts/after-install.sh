bcc0f69e576692b2c9f35970f8cc79c83f7f401f
sethm <seth@trxchange.com>
2024-02-15 21:30:37 +0000

    Step 3+4 of tutorial

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://sm-codedeploy-20240215-webappdeploymentbucket-pnw4bfdzj3xy/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
